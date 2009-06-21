b84e3d28c47d2a334d6e1d60db3d04616e70b57f
Carl F. Hall <carl.hall@gmail.com>
2009-06-21 14:07:35 -0400

    Finished clearing up the phpcs fussiness.

--- Presto.php
+++ Presto.php
@@ -1,32 +1,50 @@
 <?php
 /**
-    Copyright 2009, Carl Hall <carl.hall@gmail.com>
-
-    PHP version 5
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-       
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-    @category
-    @package
-    @author
-    @license Apache 2.0, http://www.apache.org/licenses/LICENSE-2.0
-    @link
-*/
-/**
- * Base class for handling REST based calls.
+ * Presto - a lightweight REST framework for PHP
  *
+ * Presto is a simple to use and very lightweight REST framework for PHP,
+ * it will help you to handle rest routing and input/output of data without
+ * getting in your way
+ *
+ * PHP version 5
+ *
+ * LICENSE:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @category  File
+ * @package   Presto
+ * @author    Carl Hall <carl.hall@gmail.com>
+ * @author    Aaron Zeckoski <azeckoski@gmail.com>
+ * @copyright 2009 Carl Hall
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @version   SVN: $Id:$
+ * @link      https://www.ohloh.net/p/PhpRESTo
+ * @since     inception
+ */
+define('ABSPATH', dirname(__FILE__).'/'); // the absolute path to this file
+error_reporting(E_ALL ^ E_NOTICE ^ E_USER_NOTICE); // strict error reporting
+
+/**
+ * Base class for handling REST based calls (routing and processing),
+ * This will also take care of detection of REST resource classes and execution
+ * of the methods,
  * URLs should be handled in the convention specified in the REST microformat.
  * http://microformats.org/wiki/rest/urls
+ *
+ * @category Class
+ * @package  Presto
+ * @author   Carl Hall <carl.hall@gmail.com>
+ * @author   Aaron Zeckoski <azeckoski@gmail.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link     https://www.ohloh.net/p/PhpRESTo
  */
 class RestController
 {
@@ -40,6 +58,11 @@ class RestController
         loadResources();
     }
 
+    /**
+     * Loads resources found on disc.
+     *
+     * @return void
+     */
     function loadResources()
     {
         $resources_dir = @opendir('resources')
@@ -51,11 +74,23 @@ class RestController
         }
     }
 
+    /**
+     * Display the results of dispatching the current URL.
+     *
+     * @return void
+     */
     function display()
     {
         echo $this->dispatch();
     }
 
+    /**
+     * Dispatch the current URL to the appropriate resource and return the results.
+     *
+     * @param String $url The URL to use for dispatching.
+     * 
+     * @return Results from the matched resource.
+     */
     function dispatch($url)
     {
         !empty($url) or die('Cannot dispatch without a URL.');
@@ -79,7 +114,7 @@ class RestController
         }
 
         if ($num_elements >= 2) {
-            $id = $this->get_id(urldecode($elements[1]));
+            $id = $this->getId(urldecode($elements[1]));
         }
 
         /** @TODO only check for _method when REQUEST_METHOD = (GET|POST) */
@@ -145,6 +180,9 @@ class RestController
 
     /**
      * Gets the request method performed.
+     * 
+     * @return The request method.  REQUEST_METHOD is used. If the method is GET or
+     *         POST, _method is checked on the request for override.
      */
     protected function getMethod()
     {
@@ -158,8 +196,11 @@ class RestController
     }
 
     /**
-     * Get the requested response format based on the name of the requested
-     * playlist.
+     * Get the requested response format based on a URL element.
+     * 
+     * @param String $name Resource name to check for response format.
+     * 
+     * @return Format found on the name (String after last '.').  Default is 'json'.
      */
     protected function getFormat($name)
     {
@@ -183,7 +224,14 @@ class RestController
         return $format;
     }
 
-    protected function get_id($name)
+    /**
+     * Get the ID from a URL element.
+     * 
+     * @param String $name URL element to check for ID.
+     * 
+     * @return ID found in the URL element (String before last '.').
+     */
+    protected function getId($name)
     {
         $id = '';
         $last_slash = strrpos($name, '/');
@@ -193,11 +241,11 @@ class RestController
             $last_slash = -1;
         }
 
-        // neither dot nor slash was found
         if ($last_dot === false && $last_slash === false) {
+            // neither dot nor slash was found
             $id = $name;
-        // a dot was found after a slash
         } elseif ($last_dot !== false && $last_dot > $last_slash) {
+            // a dot was found after a slash
             $id = substr($name, $last_slash + 1, $last_dot);
         } else {
             $id = substr($name, $last_slash + 1);
@@ -208,6 +256,11 @@ class RestController
 
     /**
      * Factory method to transform json into a different format.
+     * 
+     * @param String $results The results to transform.
+     * @param String $format  The format to transform to.
+     * 
+     * @return The results transformed to the request format.
      */
     function transform($results, $format)
     {
@@ -236,13 +289,20 @@ class RestController
         return $output;
     }
 
-    protected function get_class_annotations($class)
+    /**
+     * Get the annotations found on a class.
+     * 
+     * @param Class $class The class to check for annotations.
+     * 
+     * @return Associative array of annotations.
+     */
+    protected function getClassAnnotations($class)
     {
         // using reflection, get the doc comment for parsing
         $refClass = new ReflectionClass($class);
         $comment = $refClass->getDocComment();
 
-        $this->get_annotations_from_text($comment);
+        $this->getAnnotationsFromText($comment);
 
         return $annotations;
     }
@@ -250,7 +310,9 @@ class RestController
     /**
      * Parses the annotations found on methods in a class.
      *
-     * @param $class Class instance.
+     * @param Class $class Class instance to check methods for annotations.
+     * 
+     * @return Associative array of annotations on methods.
      */
     protected function getMethodsAnnotations($class)
     {
@@ -261,7 +323,7 @@ class RestController
         $methods = $refClass->getMethods();
 
         foreach ( $methods as $method ) {
-            $methodAnnotations = $this->get_annotations_from_text(
+            $methodAnnotations = $this->getAnnotationsFromText(
                 $method->getDocComment()
             );
             $annotations[$method->getName()] = $methodAnnotations;
@@ -274,9 +336,11 @@ class RestController
      * Parses the annotations from a block of text usually taken from a class
      * or method doc comment.
      *
-     * @param string $text
+     * @param String $text Text to check for annotations.
+     * 
+     * @return Associative array of annotations found in text.
      */
-    protected function get_annotations_from_text($text)
+    protected function getAnnotationsFromText($text)
     {
         // split on @ then push the first element off because it is not part of
         // the annotation.
@@ -287,6 +351,10 @@ class RestController
     }
 }
 
+
+/*
+ * Resource base class.
+ *
 class RestResource
 {
     protected $response_code = 204;
@@ -296,7 +364,7 @@ class RestResource
     /**
      *  these are only placeholders.  these will be replaced by annotation based
      *  calls.
-     */
+     *
     function _new()
     {
     }
@@ -316,4 +384,5 @@ class RestResource
     {
     }
 }
+*/
 ?>
